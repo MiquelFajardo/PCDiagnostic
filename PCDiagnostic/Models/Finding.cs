cf7f763e449911163dce6a8c166bcaca2cc4247e
Miquel Fajardo <miquel.fajardo@protonmail.com>
2026-06-24 16:34:12 +0200

    resolucio ocnfilctes

---
+++ PCDiagnostic/Models/Finding.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PCDiagnostic.Models
+{
+    public class Finding
+    {
+        public string Severity { get; set; } = "";
+
+        public string Title { get; set; } = "";
+
+        public string Description { get; set; } = "";
+
+        public string Recommendation { get; set; } = "";
+    }
+}
